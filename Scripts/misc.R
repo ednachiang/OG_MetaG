1aab1a4fa4eecc89eb7122402b1f4ea09bc145b7
ednachiang <echiang3@wisc.edu>
2020-04-09 17:04:29 -0500

    Organized misc functions to load

---
+++ Scripts/misc.R
@@ -0,0 +1,11 @@
+# Calculate standard error
+se <- function(x){
+  sd(x)/sqrt(length(x))
+}
+
+# Steal legend
+g_legend <- function(a.gplot){
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)}

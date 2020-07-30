3da751b193c0936e459e5d4dae6e73b91f4286c5
ednachiang <echiang3@wisc.edu>
2020-07-30 12:31:38 -0500

    Organized scripts to streamline "load functions" block

--- Scripts/misc.R
+++ Scripts/misc.R
@@ -1,11 +1,28 @@
-# Calculate standard error
+### Calculate standard error
 se <- function(x){
   sd(x)/sqrt(length(x))
 }
 
-# Steal legend
+### Steal legend
 g_legend <- function(a.gplot){
   tmp <- ggplot_gtable(ggplot_build(a.gplot))
   leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
   legend <- tmp$grobs[[leg]]
   return(legend)}
+
+### DESeq2
+deSEQ <- function(data, valuetest, cutoff = 0, alpha = 0.05){
+  data_pruned <- prune_taxa(taxa_sums(data) > cutoff, data)
+  de_data <- phyloseq_to_deseq2(data_pruned, valuetest)
+  de_data2 <- DESeq(de_data, test="Wald", fitType="local")
+  res_data <- results(de_data2, cooksCutoff = FALSE, pAdjustMethod = "BH")
+  sig_data <- res_data[which(res_data$padj < alpha), ]
+  sigtab_sherm <- cbind(as(sig_data, "data.frame"), as(tax_table(data_pruned)[rownames(sig_data), ], "matrix"))
+}
+
+### NMDS Ellipses
+veganCovEllipse <- function(cov, center = c(0, 0), scale = 1, npoints = 100) {
+  theta <- (0:npoints) * 2 * pi/npoints
+  Circle <- cbind(cos(theta), sin(theta))
+  t(center + scale * t(Circle %*% chol(cov)))
+}

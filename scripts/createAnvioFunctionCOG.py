351d70f76794efbfc8744c061b4e5a85dee4c3e1
ednachiang <echiang3@wisc.edu>
2021-03-03 20:01:35 -0600

    Renamed createAnvioFunction.py

--- scripts/createAnvioFunctionCOG.py
+++ scripts/createAnvioFunctionCOG.py
@@ -9,12 +9,6 @@ directoryGeneIDinput = 'test_createAnvioFunction/geneID/'
 directoryOutput = 'test_createAnviofunction/output/'
 
 
-def convert_list_to_string(org_list, seperator=' '):
-    #""" Convert list to string, by joining all item in list with given separator.
-    #    Returns the concatenated string """
-    return seperator.join(org_list)
-
-
 def makeFunctionTable(inputCOG, inputID):
     input_COG = open(inputCOG, mode = 'r')
     input_ID = open(inputID, mode = 'r')
